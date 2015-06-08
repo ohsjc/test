ef9b32df6bafee92e389c1e3b70a0c53fca88e9e
ohsjc <ohsjeremyc@gmail.com>
2015-06-08 10:50:04 -0600

    i'm testing some things

--- o.js
+++ o.js
@@ -9,10 +9,30 @@ function sayHello (words){
 var start = sayHello("My name is Jeremy.");
 console.log(start("Its nice to meet you."));
 
-console.log("testing func params");
+//testing func params
 
 function readMeParams(first, second){
   return second;
 }
 
 console.log(readMeParams("There is only one"));
+
+//testing mutability + scopes
+
+function Cat(color, disposition, favToy){
+  this.fcolor = function(){ return color;};
+  this.fdisposition = function(){ return disposition;};
+  this.ffavToy = function(){ return favToy;};
+}
+
+function catChecker(cat){
+  return ("This cat is " + cat.fcolor() + ", is very " + cat.fdisposition() + "and love's its " + cat.ffavToy());
+}
+
+var kitty = new Cat("Black", "Cute", "Mouse");
+var grumpyKitty = new Cat("White", "Fussy", "Ball");
+
+console.log(catChecker(kitty));
+kitty.color = "orange";
+console.log(catChecker(kitty));
+console.log(kitty.color, kitty.fcolor());
